1c1d42dcae60e26531f9dfe89f294debbab0089e
huttatran <hdtran114@student.hbuhsd.edu>
2025-10-07 21:45:00 +0000

    as

--- Main.java
+++ Main.java
@@ -4,6 +4,15 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    int num1 = 10;
+    int num2 = 4;
+    Rectangle rect1 = new Rectangle(num1, num2);
+    Rectangle rect2 = new Rectangle(num2, num1);
+    boolean isRotated = (rect1.getLength() == rect2.getWidth() && rect1.getWidth() == rect2.getLength());
+    System.out.println(isRotated);
+    boolean isCongruent = (isRotated == true || rect1.equals(rect2));
+    System.out.println(isCongruent);
+    boolean isSimilar = (isCongruent == true || rect2.getLength()/rect1.getLength() == rect2.getWidth()/rect1.getWidth());
+    System.out.println(isSimilar);
   }
 }
